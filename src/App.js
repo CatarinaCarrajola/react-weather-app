299e2d89b58cf9e3c52dc9afdcd89e7ead8bd4c0
CatarinaCarrajola <catarinacarrajola+github@gmail.com>
2021-11-01 21:34:29 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Catarina Carrajola and is{" "}
+        <a
+          href="https://github.com/CatarinaCarrajola/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
