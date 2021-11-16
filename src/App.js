a36deaac51b40c2d08449c1f12bbcc8ae5ad0dcc
CatarinaCarrajola <catarinacarrajola+github@gmail.com>
2021-11-16 14:04:05 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Catarina Carrajola and is{" "}
-        <a
-          href="https://github.com/CatarinaCarrajola/react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Catarina Carrajola and is{" "}
+          <a
+            href="https://github.com/CatarinaCarrajola/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
