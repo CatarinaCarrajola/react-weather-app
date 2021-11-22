2d430201dddec13fbb1e22e6939266f70673ba6d
CatarinaCarrajola <catarinacarrajola+github@gmail.com>
2021-11-22 15:35:10 +0100

    Debugging 2
    Hosted successfully, however website didn't go past the loading phase. Attempt on debugging by changing "http" to "https" on apiURL.

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "7ef5f0ed13623c4b72c2365c9ec2833c";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
