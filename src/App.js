723a3a6bfc2a7b1c9021470fe589493dceab1caa
CatarinaCarrajola <catarinacarrajola+github@gmail.com>
2021-11-24 14:39:56 +0100

    Added 1 Day Forecast

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="London" />
+
         <footer>
           This project was coded by Catarina Carrajola and is{" "}
           <a
